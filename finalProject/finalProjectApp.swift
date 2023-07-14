d3e50b241ded47d798090798af2f9369638fd648
Prithika <prithikavenkatesh@gmail.com>
2023-07-15 02:11:49 +0800

    Initial Commit

---
+++ finalProject/finalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalProjectApp.swift
+//  finalProject
+//
+//  Created by Prithika Venkatesh on 15/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct finalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
