e1e385aa6b90a796f7e1c79f104ff3bea21b5ac1
Noga <nogah0317@gmail.com>
2023-07-20 19:20:04 -0400

    new changes to homepage

--- finalProject/ContentView.swift
+++ finalProject/ContentView.swift
@@ -11,54 +11,58 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             ZStack {
+                Color(red: 246/255, green: 240/255, blue: 214/255)
+                    .ignoresSafeArea()
                 VStack {
-                    Text("type short info abt app")
-                    Spacer()
-                    
-                    
                     NavigationLink(destination: calenderPage()) {
                         ZStack {
-                                Capsule()
-                                    .fill(Color.green)
-                                    .frame(width: 437.5, height: 187.5)
-
-                                Text("Calendar")
-                                    .foregroundColor(.black)
-                                    .font(.largeTitle)
-                                    .offset(x:80)
-                                
-                            }
-                        }
-                    .offset(x:-180, y:-105)
-                    
-                    
-                            NavigationLink(destination: resourcesPage()) {
-                                ZStack {
-                                    Capsule()
-                                        .fill(Color.purple)
-                                        .frame(width: 437.5, height: 187.5)
-                                       
-                                    Text("Resources")
-                                        .foregroundColor(.black)
-                                        .font(.largeTitle)
-                                        .offset(x:-95, y:0)
-                                }
-                            }
-                            .offset(x:190, y:-130)
+                            Capsule()
+                                .fill( Color(red: 172/255, green: 214/255, blue: 87/255))
+                                .frame(width: 437.5, height: 187.5)
                             
-                            NavigationLink(destination: recipesPage()) {
-                                ZStack {
-                                    Capsule()
-                                        .fill(Color.blue)
-                                        .frame(width: 437.5, height: 187.5)
-                                                                       
-                                    Text("Recipes")
-                                        .foregroundColor(.black)
-                                        .font(.largeTitle)
-                                        .offset(x: 75 , y: 0)
-                                }
-                            }
-                    .offset(x:-190, y:-160)
+                            Text("Calendar")
+                                .foregroundColor(.black)
+                                .font(.largeTitle)
+                                .offset(x:80)
+                            
+                        }
+                    }
+                    .offset(x:-180, y:-15)
+                    
+                    
+                    NavigationLink(destination: resourcesPage()) {
+                        ZStack {
+                            Capsule()
+                                .fill(Color(red: 149/255, green: 214/255, blue: 255/255))
+                                .frame(width: 437.5, height: 187.5)
+                            
+                            Text("Resources")
+                                .foregroundColor(.black)
+                                .font(.largeTitle)
+                                .offset(x:-95, y:0)
+                        }
+                    }
+                    .offset(x:190, y:-130)
+                    
+                    NavigationLink(destination: recipesPage()) {
+                        ZStack {
+                            Capsule()
+                                .fill(Color(red: 230/255, green: 146/255, blue: 123/255))
+                                .frame(width: 437.5, height: 187.5)
+                            
+                            Text("Recipes")
+                                .foregroundColor(.black)
+                                .font(.largeTitle)
+                                .offset(x: 75 , y: 0)
+                        }
+                    }
+                    .offset(x:-190, y:50)
+                    
+                   
+                    NavigationLink(destination: ContentView()) {
+
+                        Image("homePage")
+                            .aspectRatio(contentMode: .fit)
                     }
                 }
             }
@@ -70,4 +74,4 @@ struct ContentView: View {
             ContentView()
         }
     }
-
+}
